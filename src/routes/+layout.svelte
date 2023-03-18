ecf098f4afedb4cd6aba20132edc2d2b29e6437c
hessam_khoobkar <41147418+hessamkhoobkar@users.noreply.github.com>
2023-03-18 21:44:14 +0330

    base layout styles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,21 @@
 <script lang="ts">
 	import '../app.css';
+	import Sidebar from '../components/Sidebar.svelte';
 </script>
 
-<slot />
+<div class="flex justify-start items-start h-screen p-2 gap-2">
+	<Sidebar />
+
+	<div class="grow h-full">
+		<slot />
+	</div>
+</div>
 
 <style lang="postcss">
-	/* :global(html) {
-		background-color: theme(colors.gray.100);
-	} */
+	:global(html) {
+		color: theme(colors.gray.100);
+		font-size: theme(fontSize.base);
+		font-weight: theme(fontWeight.medium);
+		background-color: theme(colors.zinc.900);
+	}
 </style>
