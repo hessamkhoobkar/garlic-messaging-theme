f958fa6e5da40170111eae60c25b6e95e95e83e5
hessam_khoobkar <41147418+hessamkhoobkar@users.noreply.github.com>
2023-03-18 21:44:28 +0330

    init main screen

---
+++ src/routes/selected/+page.svelte
@@ -0,0 +1,6 @@
+<div class="h-full w-full flex flex-col justify-start items-start">
+	<div class="w-full h-14 rounded-2xl bg-zinc-850">header</div>
+	<div class="">
+		<p>Text goes here</p>
+	</div>
+</div>
