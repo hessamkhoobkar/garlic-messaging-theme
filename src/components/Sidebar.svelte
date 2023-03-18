a3b6d8aec569357424156119a19859c203365d42
hessam_khoobkar <41147418+hessamkhoobkar@users.noreply.github.com>
2023-03-18 21:44:01 +0330

    init sidebard

---
+++ src/components/Sidebar.svelte
@@ -0,0 +1,9 @@
+<script>
+	import ChatsCard from './sidebar/ChatsCard.svelte';
+	import UserCard from './sidebar/UserCard.svelte';
+</script>
+
+<div class="w-80 h-full flex flex-col justify-start items-start gap-2">
+	<UserCard />
+	<ChatsCard />
+</div>
---
+++ src/components/sidebar/ChatsCard.svelte
@@ -0,0 +1,7 @@
+<div class="w-80 grow bg-zinc-850 rounded-2xl p-4 flex flex-col justify-start items-start">
+	<span>Here is a name</span>
+	<span>Here is a name</span>
+	<span>Here is a name</span>
+	<span>Here is a name</span>
+	<span>Here is a name</span>
+</div>
