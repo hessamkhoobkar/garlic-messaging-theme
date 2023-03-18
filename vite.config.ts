8ff42f9d2a7e3d33ac8f3c85f233c1bb3b0c7949
hessam_khoobkar <41147418+hessamkhoobkar@users.noreply.github.com>
2023-03-18 21:42:34 +0330

    add alias to project

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import { resolve } from 'path';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	resolve: {
+		alias: {
+			'@': resolve(__dirname, 'src'),
+			'@components': resolve(__dirname, 'src/components'),
+			'@utils': resolve(__dirname, 'src/utils')
+		}
+	},
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
