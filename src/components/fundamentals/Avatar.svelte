54ba58f3d2d4c7d4b632f537be619a837f837ae4
hessam_khoobkar <41147418+hessamkhoobkar@users.noreply.github.com>
2023-03-18 21:43:27 +0330

    init avatar

---
+++ src/components/fundamentals/Avatar.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	let klass: string | null = null;
+	export { klass as class };
+	export let src: string | null = null;
+	export let alt: string | null = null;
+	export let size: '36' | '42' | '44' | '126' = '42';
+</script>
+
+<div class="bg-zinc-700 rounded-full">
+	<img {src} {alt} height={size} width={size} class="rounded-full {klass}" />
+</div>
