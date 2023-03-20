db23d7b5256244a1b041829694cae64f4a11c751
hessam_khoobkar <41147418+hessamkhoobkar@users.noreply.github.com>
2023-03-20 12:53:05 +0330

    add chat chat input and init chat panel components

---
+++ src/components/ChatPanel.svelte
@@ -0,0 +1,135 @@
+<div class="w-full grow overflow-y-auto text-zinc-800">
+	<p>
+		Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio veniam soluta id omnis unde
+		minima obcaecati nobis eos totam, sint laudantium sed alias vero tenetur aspernatur, temporibus
+		assumenda quos doloribus.
+	</p>
+	<p>
+		Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio veniam soluta id omnis unde
+		minima obcaecati nobis eos totam, sint laudantium sed alias vero tenetur aspernatur, temporibus
+		assumenda quos doloribus.
+	</p>
+	<p>
+		Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio veniam soluta id omnis unde
+		minima obcaecati nobis eos totam, sint laudantium sed alias vero tenetur aspernatur, temporibus
+		assumenda quos doloribus.
+	</p>
+	<p>
+		Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio veniam soluta id omnis unde
+		minima obcaecati nobis eos totam, sint laudantium sed alias vero tenetur aspernatur, temporibus
+		assumenda quos doloribus.
+	</p>
+	<p>
+		Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio veniam soluta id omnis unde
+		minima obcaecati nobis eos totam, sint laudantium sed alias vero tenetur aspernatur, temporibus
+		assumenda quos doloribus.
+	</p>
+	<p>
+		Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio veniam soluta id omnis unde
+		minima obcaecati nobis eos totam, sint laudantium sed alias vero tenetur aspernatur, temporibus
+		assumenda quos doloribus.
+	</p>
+	<p>
+		Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio veniam soluta id omnis unde
+		minima obcaecati nobis eos totam, sint laudantium sed alias vero tenetur aspernatur, temporibus
+		assumenda quos doloribus.
+	</p>
+	<p>
+		Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio veniam soluta id omnis unde
+		minima obcaecati nobis eos totam, sint laudantium sed alias vero tenetur aspernatur, temporibus
+		assumenda quos doloribus.
+	</p>
+	<p>
+		Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio veniam soluta id omnis unde
+		minima obcaecati nobis eos totam, sint laudantium sed alias vero tenetur aspernatur, temporibus
+		assumenda quos doloribus.
+	</p>
+	<p>
+		Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio veniam soluta id omnis unde
+		minima obcaecati nobis eos totam, sint laudantium sed alias vero tenetur aspernatur, temporibus
+		assumenda quos doloribus.
+	</p>
+	<p>
+		Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio veniam soluta id omnis unde
+		minima obcaecati nobis eos totam, sint laudantium sed alias vero tenetur aspernatur, temporibus
+		assumenda quos doloribus.
+	</p>
+	<p>
+		Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio veniam soluta id omnis unde
+		minima obcaecati nobis eos totam, sint laudantium sed alias vero tenetur aspernatur, temporibus
+		assumenda quos doloribus.
+	</p>
+	<p>
+		Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio veniam soluta id omnis unde
+		minima obcaecati nobis eos totam, sint laudantium sed alias vero tenetur aspernatur, temporibus
+		assumenda quos doloribus.
+	</p>
+	<p>
+		Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio veniam soluta id omnis unde
+		minima obcaecati nobis eos totam, sint laudantium sed alias vero tenetur aspernatur, temporibus
+		assumenda quos doloribus.
+	</p>
+	<p>
+		Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio veniam soluta id omnis unde
+		minima obcaecati nobis eos totam, sint laudantium sed alias vero tenetur aspernatur, temporibus
+		assumenda quos doloribus.
+	</p>
+	<p>
+		Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio veniam soluta id omnis unde
+		minima obcaecati nobis eos totam, sint laudantium sed alias vero tenetur aspernatur, temporibus
+		assumenda quos doloribus.
+	</p>
+	<p>
+		Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio veniam soluta id omnis unde
+		minima obcaecati nobis eos totam, sint laudantium sed alias vero tenetur aspernatur, temporibus
+		assumenda quos doloribus.
+	</p>
+	<p>
+		Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio veniam soluta id omnis unde
+		minima obcaecati nobis eos totam, sint laudantium sed alias vero tenetur aspernatur, temporibus
+		assumenda quos doloribus.
+	</p>
+	<p>
+		Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio veniam soluta id omnis unde
+		minima obcaecati nobis eos totam, sint laudantium sed alias vero tenetur aspernatur, temporibus
+		assumenda quos doloribus.
+	</p>
+	<p>
+		Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio veniam soluta id omnis unde
+		minima obcaecati nobis eos totam, sint laudantium sed alias vero tenetur aspernatur, temporibus
+		assumenda quos doloribus.
+	</p>
+	<p>
+		Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio veniam soluta id omnis unde
+		minima obcaecati nobis eos totam, sint laudantium sed alias vero tenetur aspernatur, temporibus
+		assumenda quos doloribus.
+	</p>
+</div>
+
+<style lang="postcss">
+	:root {
+		--scrollbarBG: transparent;
+		--scrollbar: theme(colors.zinc.800);
+	}
+
+	* {
+		scrollbar-width: thin;
+		scrollbar-color: var(--scrollbarBG) var(--scrollbar);
+	}
+
+	*::-webkit-scrollbar {
+		width: 8px;
+	}
+
+	*::-webkit-scrollbar-track {
+		background: var(--scrollbarBG);
+		margin-top: 0.5rem;
+		margin-bottom: 0.5rem;
+	}
+
+	*::-webkit-scrollbar-thumb {
+		background-color: var(--scrollbar);
+		border-radius: 20px;
+		border: 2px solid var(--scrollbarBG);
+	}
+</style>
