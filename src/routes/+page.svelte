0a2d7fb8bc20ebe86f5e1f0b76c6c0af6f0da988
hessam_khoobkar <41147418+hessamkhoobkar@users.noreply.github.com>
2023-03-20 11:45:32 +0330

    init header card for caht section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,20 @@
 <script lang="ts">
 	import { activeChat } from '@utils/store';
+	import { selectedUser } from '@data/selectedUser.mock';
+	import ChatHeader from '@components/ChatHeader.svelte';
 </script>
 
 {#if $activeChat}
-	<div class="h-full w-full flex justify-center items-center">
-		<p>Chat selected</p>
+	<div class="h-full w-full flex justify-start items-start">
+		<div class="h-full w-full flex flex-col justify-start items-start">
+			<ChatHeader
+				avatar={selectedUser.avatar}
+				name={selectedUser.name}
+				activityState={selectedUser.activityState}
+			/>
+
+			<p>Chat selected</p>
+		</div>
 	</div>
 {:else}
 	<div class="h-full w-full flex justify-center items-center">
