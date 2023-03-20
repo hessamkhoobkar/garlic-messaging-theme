2dcb6f129e99d9f5399e54b3ad9dbc9827080317
hessam_khoobkar <41147418+hessamkhoobkar@users.noreply.github.com>
2023-03-20 12:53:34 +0330

    import chat components and add base transitions

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,33 @@
 <script lang="ts">
+	import { fly } from 'svelte/transition';
+	import { expoInOut } from 'svelte/easing';
 	import { activeChat } from '@utils/store';
 	import { selectedUser } from '@data/selectedUser.mock';
+
 	import ChatHeader from '@components/ChatHeader.svelte';
+	import ChatInput from '@components/ChatInput.svelte';
+	import ChatPanel from '@components/ChatPanel.svelte';
 </script>
 
-{#if $activeChat}
-	<div class="h-full w-full flex justify-start items-start">
-		<div class="h-full w-full flex flex-col justify-start items-start">
-			<ChatHeader
-				avatar={selectedUser.avatar}
-				name={selectedUser.name}
-				activityState={selectedUser.activityState}
-			/>
-
-			<p>Chat selected</p>
+<div class="h-full w-full overflow-hidden">
+	{#if $activeChat}
+		<div
+			class="h-full w-full flex justify-start items-start"
+			transition:fly={{ duration: 1000, easing: expoInOut, x: 150 }}
+		>
+			<div class="h-full w-full flex flex-col justify-start items-start">
+				<ChatHeader
+					avatar={selectedUser.avatar}
+					name={selectedUser.name}
+					activityState={selectedUser.activityState}
+				/>
+				<ChatPanel />
+				<ChatInput />
+			</div>
 		</div>
-	</div>
-{:else}
-	<div class="h-full w-full flex justify-center items-center">
-		<p>No chat selected, please select on</p>
-	</div>
-{/if}
+	{:else}
+		<div class="h-full w-full flex justify-center items-center">
+			<p>No chat selected, please select on</p>
+		</div>
+	{/if}
+</div>
