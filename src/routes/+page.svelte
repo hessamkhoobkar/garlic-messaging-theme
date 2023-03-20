48df05d9ddcd0b5c594563b52467ac136a7fe751
hessam_khoobkar <41147418+hessamkhoobkar@users.noreply.github.com>
2023-03-20 10:28:28 +0330

    move back SPA approach

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,15 @@
-<div class="h-full w-full flex justify-center items-center">
-	<p>No chat selected, please select on</p>
-</div>
+<script lang="ts">
+	let activeChat: boolean = false;
+</script>
+
+{#if activeChat}
+	<!-- content here -->
+	<div class="h-full w-full flex justify-center items-center">
+		<p>Chat selected</p>
+	</div>
+{:else}
+	<!-- else content here -->
+	<div class="h-full w-full flex justify-center items-center">
+		<p>No chat selected, please select on</p>
+	</div>
+{/if}
