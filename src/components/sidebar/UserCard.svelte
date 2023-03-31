0a2a5cb61e647af8aeb549def8b52293cc0f48f8
hessam_khoobkar <41147418+hessamkhoobkar@users.noreply.github.com>
2023-03-31 19:55:01 +0330

    add user details state

--- src/components/sidebar/UserCard.svelte
+++ src/components/sidebar/UserCard.svelte
@@ -1,9 +1,15 @@
-<script>
+<script lang="ts">
 	import Button from '@components/fundamentals/Button/Button.svelte';
 	import Avatar from '@components/fundamentals/Avatar.svelte';
+	import UserDetail from '@components/sidebar/UserDetail.svelte';
 
 	import ChatCircle from '@/assets/icons/ChatCircleDuotone.svelte';
 	import currentUser from '@/assets/imgs/current-user.jpg';
+
+	let isActive: boolean = false;
+	function viewUserDetail() {
+		isActive = !isActive;
+	}
 </script>
 
 <div class="w-80 bg-zinc-850 rounded-2xl p-4 gap-4 flex flex-col justify-between items-start">
@@ -11,9 +17,12 @@
 		<h2 class="text-lg font-medium tracking-[-0.02em] mb-2">Messages</h2>
 		<button
 			class="rounded-full border-4 border-transparent hover:border-4 hover:border-zinc-600 transition-all duration-200 ease-in-out"
+			on:click={viewUserDetail}
 		>
 			<Avatar src={currentUser} alt="user's avatar" />
 		</button>
 	</div>
 	<Button label="Start new chat" icon={ChatCircle} class="" />
 </div>
+
+<UserDetail {isActive} on:close={viewUserDetail} />
