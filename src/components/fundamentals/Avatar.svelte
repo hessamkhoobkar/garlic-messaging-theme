4f26c34a6dd98aa61cf737864deebb7c079a92a9
hessam_khoobkar <41147418+hessamkhoobkar@users.noreply.github.com>
2023-03-31 19:54:26 +0330

    update avatar sizes

--- src/components/fundamentals/Avatar.svelte
+++ src/components/fundamentals/Avatar.svelte
@@ -3,9 +3,9 @@
 	export { klass as class };
 	export let src: string;
 	export let alt: string;
-	export let size: '36' | '42' | '44' | '126' = '42';
+	export let size: '36' | '42' | '44' | '80' | '126' = '42';
 </script>
 
-<div class="bg-zinc-700 rounded-full" style="height: {size}px; width: {size}px;">
+<div class="bg-zinc-700 rounded-full " style="height: {size}px; width: {size}px;">
 	<img {src} {alt} height={size} width={size} class="rounded-full {klass}" />
 </div>
